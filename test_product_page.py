2bc406c70da0ec12cf0ad8ad3a81a24ef33b9064
Natalia Boichuk <boichuk.n.m@gmail.com>
2021-06-12 20:53:49 +0300

    add login in base_page, and tests

--- test_product_page.py
+++ test_product_page.py
@@ -1,7 +1,4 @@
-import time
-
 import pytest
-
 from .pages.product_page import ProductPage
 
 link_ = ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
@@ -15,6 +12,20 @@ link_ = ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?pr
                       marks=pytest.mark.xfail),
          "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
          "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"]
+
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
 
 
 # @pytest.mark.parametrize('link', link_)
